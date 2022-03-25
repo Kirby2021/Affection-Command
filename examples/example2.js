a7870a5638dfe46b6fbd272412cea53db1b20df6
yung <56764505+yungbeatz@users.noreply.github.com>
2022-03-25 22:57:00 +0100

    Create example2.js

---
+++ examples/example2.js
@@ -0,0 +1,40 @@
+const { MessageEmbed } = require("discord.js");
+const rga = require("random-gif-api")
+const fetch = require('node-fetch');
+
+
+
+module.exports = {
+    name: "kiss",
+    description: "kiss someone!",
+    options: [
+        {
+            name: "member",
+            description: "Mention the user you want to kiss",
+            type: "USER",
+            required: true,
+            
+        }
+    ],
+    async execute(interaction) {
+        let Target = interaction.options.getMember("member");
+        let Executer = interaction.user;
+        let data = await rga.getGif('kiss');
+        let HugEmbed = new MessageEmbed()
+        .setColor('#000000')
+        .setDescription(`**${Executer} kisses ${Target}**`)
+        .setImage(data)
+        .setTimestamp()
+        console.log(data);
+      
+       if(Target.id === Executer.id) {
+        Response = new MessageEmbed()
+        .setColor('#000000')
+        .setDescription("⛔ You cant kiss yourself! But is you ok? Do you need to vent?")
+            return interaction.reply({ embeds: [Response] })
+        } else {
+        interaction.reply({ embeds: [HugEmbed]}); 
+        }
+    }
+}
+

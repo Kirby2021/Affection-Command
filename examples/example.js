a0d3db97a64abfe6360c024359690406660a2416
yung <56764505+yungbeatz@users.noreply.github.com>
2022-03-25 22:52:32 +0100

    Create example.js

---
+++ examples/example.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require("discord.js");
+const rga = require("random-gif-api")
+const fetch = require('node-fetch');
+
+
+
+module.exports = {
+    name: "hug",
+    description: "hug someone!",
+  
+    options: [
+        {
+            name: "member",
+            description: "Mention the user you want to hug",
+            type: "USER",
+            required: true,
+            
+        }
+    ],
+    async execute(interaction) {
+        let Target = interaction.options.getMember("member");
+        let Executer = interaction.user;
+        let data = await rga.getGif('cuddle');
+        let HugEmbed = new MessageEmbed()
+        .setColor('#000000')
+        .setDescription(`**${Executer} gives a big hug to ${Target}**`)
+        .setImage(data)
+        .setTimestamp()
+
+        console.log(data);
+      
+       if(Target.id === Executer.id) {
+        Response = new MessageEmbed()
+        .setColor('#000000')
+        .setDescription("⛔ You cant hug yourself! But is you ok? Do you need to vent?")
+            return interaction.reply({ embeds: [Response] })
+        } else {
+        interaction.reply({ embeds: [HugEmbed]}); 
+        }
+    }
+}
+
